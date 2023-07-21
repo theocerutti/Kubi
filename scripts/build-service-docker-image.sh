116e37eba4ef2253a4e8c4837e4178726e88051c
Théo Cerutti <tepiartheo@gmail.com>
2023-07-21 14:06:03 +0200

    Add github actions build docker images

--- scripts/build-service-docker-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t backend:latest ../back
-docker build -t frontend:latest ../front
-docker build -t indexer:latest ../indexer
-docker build -t reporting:latest ../reporting
-
